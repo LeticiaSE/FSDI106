be0f566742925e9290660dcaafeb84afacb36a5b
LeticiaSE <35822181+LeticiaSE@users.noreply.github.com>
2020-07-26 20:05:02 -0700

    Online Store
    GET
    POST
    - Displays the catalog of products
    - Allow the admin to create new products
    - Uses Ajax communication to send and retrieve the products from a back end.
         back end url:  http://restclass.azurewebsites.net/API/points (Links to an external site.)

--- OnlineStore/Script/admin.js
+++ OnlineStore/Script/admin.js
@@ -1,3 +1,28 @@
+/*
+    AJAX
+    Async Javascript and Xml
+
+    http://restclass.azurewebsites.net
+    http verbs (request Methods)
+        GET: get info (CAN NOT SEND DATA)
+        POST: create/send data
+        PUT: update some existing elements
+        PATCH: update part of an existing element
+        DELETE: remove an existing element
+
+*/
+
+//Object Constructor for Item
+function Item(code,title,price,category,image) {
+    this.code = code;
+    this.title = title;
+    this.price = price;
+    this.category = category;
+    this.image = image;
+    this.user = 'Lety';
+    
+}
+
 function register() {
     var code = $("#txtCode").val();
     var title = $("#txtTitle").val();
@@ -5,7 +30,25 @@ function register() {
     var category = $("#txtCategory").val();
     var image = $("#txtImage").val();
 
-    console.log(code, title, category, price, image);
+    var item = new Item(code,title,price,category,image);
+    console.log(item);
+    console.log(JSON.stringify(item));
+
+    //console.log(code, title, category, price, image);
+
+    //create the AJAX request
+    $.ajax({
+        url: 'http://restclass.azurewebsites.net/api/points',
+        type: 'POST',
+        data: JSON.stringify(item),//specify the object
+        contentType: 'application/json',
+        success: function(response) {
+            console.log("Yeei", response);
+        },
+        error: function(errorDetails) {
+            console.log("Ouuch.!",errorDetails);
+        }
+    });
 }
 
 function init() {
@@ -16,3 +59,4 @@ function init() {
 
 
 window.onload = init;
+
